b943762a348f298ec37d26a4ee473c9da7b0362d
Geoffrey Brophy <geoffrey.brophy@proton.me>
2025-03-05 20:51:01 -0500

    initial commit

---
+++ core/src/main/Java/com/yesiamanerd/Main.java
@@ -0,0 +1,13 @@
+package com.yesiamanerd;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        log.info("Guess The Number Game");
+    }
+}
